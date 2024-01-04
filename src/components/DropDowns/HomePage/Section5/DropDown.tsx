e29d2bfa0c63270137eb0295c9865514cedf9385
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2024-01-04 14:31:11 +0530

    home page continuing -8

---
+++ src/components/DropDowns/HomePage/Section5/DropDown.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+const DropDown = () => {
+  return (
+    <button className="cursor-pointer group block border-b border-[#181818] pb-4 focus:border-[#444444] transition-all">
+      <span className="flex items-center justify-between">
+        <span className="flex items-center gap-10">
+          <span className="block">
+            <Image
+              src={"/images/s1.svg"}
+              alt="s1 not loading..."
+              width={500}
+              height={500}
+              className="md:w-[50px] w-[40px]"
+            />
+          </span>
+          <span className="block text-base md:text-2xl">
+            Full-Stack Development
+          </span>
+        </span>
+        <span className="flex items-center justify-center relative duration-1000 transition-all group-focus:-rotate-90">
+          <span className="w-[15px] md:w-[25px] absolute bg-white h-[1px] transition-all duration-500 group-focus:bg-transparent"></span>
+          <span className="w-[15px] md:w-[25px] absolute bg-white h-[1px] -rotate-90 transition-all"></span>
+          <span className="w-[15px] md:w-[25px] block h-[1px] rotate-90 transition-all"></span>
+        </span>
+      </span>
+      <span className="block text-left overflow-hidden group-focus:h-[100px] h-0 duration-1000 transition-all ">
+        <span className="px-5 text-gray-400 text-xs md:text-lg pt-7 pb-10 block">
+          The artistry and precision of Full-Stack Development. From crafting
+          interactive front-end experiences that captivate users to architecting
+          robust back-end systems.
+        </span>
+      </span>
+    </button>
+  );
+};
+
+export default DropDown;
---
+++ src/components/Sections/HomePage/Section5.tsx
@@ -0,0 +1,26 @@
+import DropDown from "@/components/DropDowns/HomePage/Section5/DropDown";
+import Image from "next/image";
+import React from "react";
+
+const Section5 = () => {
+  return (
+    <div className="flex w-full mb-20 min-h-[50vh] justify-between md:px-10 md:flex-row flex-col">
+      <div className="md:w-[35%] flex flex-col justify-between gap-5">
+        <p className="text-6xl font-[400]">What I Do</p>
+        <p className="text-lg text-gray-400">
+          I specialize in crafting custom solutions that seamlessly blend
+          innovation with functionality.
+        </p>
+      </div>
+      <div className="w-full md:w-[60%] space-y-10 md:my-0 my-10">
+        <DropDown />
+        <DropDown />
+        <DropDown />
+        <DropDown />
+        <DropDown />
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
